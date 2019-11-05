b24b57dadb355036cf2d9cf930869bfb4fd0d1ca
catharina moral <catharina.moral@gmail.com>
2019-11-05 16:30:44 -0200

    b

--- Controllers/EventoTblController.cs
+++ Controllers/EventoTblController.cs
@@ -49,7 +49,7 @@ namespace PROJETO.Controllers {
             return listaRetornada;
         }
 
-        [HttpGet ("status/(status)")]
+        [HttpGet ("status/{status}")]
         public async Task<List<EventoTbl>> GetStatus (string status){
             List<EventoTbl> listaRetornada = await repositorio.BuscarPorStatus(status);
             return listaRetornada;
